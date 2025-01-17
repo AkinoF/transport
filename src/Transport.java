17b5e57a82c9bcb48427737355afc4bdaf522eea
AkinoF <147878497+AkinoF@users.noreply.github.com>
2025-01-17 10:50:09 +0300

    переделал класс транспорт

--- src/Transport.java
+++ src/Transport.java
@@ -1,79 +1,77 @@
+import java.io.*;
 import java.util.*;
+
 public class Transport {
     public static void main(String[] args) {
-        // Коллекция автомобилей
+        // Коллекция автомобилей и самолетов
         List<Car> cars = new ArrayList<>();
-        cars.add(new Car("A123BC", "Toyota", 20000, 150, 50000, true));
-        cars.add(new Car("B456DE", "BMW", 30000, 200, 30000, false));
-        cars.add(new Car("C789FG", "Mercedes", 50000, 250, 10000, true));
-
-        // Коллекция самолетов
         List<Airplane> airplanes = new ArrayList<>();
-        airplanes.add(new Airplane("P1234", "Boeing", 500000, 80000, 12000));
-        airplanes.add(new Airplane("P5678", "Airbus", 600000, 85000, 13000));
-        airplanes.add(new Airplane("P9101", "Cessna", 200000, 20000, 6000));
 
-        // Найти стоимость и пробег заданного автомобиля
-        String searchCarNumber = "B456DE";
+        // Чтение автомобилей из файла
+        readCarsFromFile("C:\\Project\\nikit2-2IS\\TransportFile\\RF\\cars.txt", cars);
+
+        // Чтение самолетов из файла
+        readAirplanesFromFile("C:\\Project\\nikit2-2IS\\TransportFile\\RF\\airplanes.txt", airplanes);
+
+        // Получение информации обо всех автомобилях, которые не прошли техосмотр
+        List<Car> carsNotPassedTechInspection = new ArrayList<>();
         for (Car car : cars) {
-            if (car.getNumber().equals(searchCarNumber)) {
-                System.out.println("Стоимость: " + car.getCost() + ", Пробег: " + car.getMileage());
-                break;
+            if (!car.isTechInspectionPassed()) {
+                carsNotPassedTechInspection.add(car);
             }
         }
 
-        // Подсчет налога с регистрации всех машин (5% стоимости)
-        double carTax = 0;
-        for (Car car : cars) {
-            carTax += car.getCost() * 0.05;
-        }
-        System.out.println("Налог с регистрации всех машин: " + carTax);
+        // Запись информации о непрошедших техосмотр автомобилях в файл
+        writeCarsNotPassedTechInspectionToFile("cars_not_passed_tech_inspection.txt", carsNotPassedTechInspection);
 
-        // Проверка, прошел ли техосмотр владелец самой дорогой машины
-        Car mostExpensiveCar = cars.get(0);
-        for (Car car : cars) {
-            if (car.getCost() > mostExpensiveCar.getCost()) {
-                mostExpensiveCar = car;
+        // Вывод общего количества прочитанных автомобилей и самолетов
+        System.out.println("Количество автомобилей: " + cars.size());
+        System.out.println("Количество самолетов: " + airplanes.size());
+    }
+
+    private static void readCarsFromFile(String filename, List<Car> cars) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                String number = parts[0];
+                String brand = parts[1];
+                double cost = Double.parseDouble(parts[2]);
+                double power = Double.parseDouble(parts[3]);
+                double mileage = Double.parseDouble(parts[4]);
+                boolean techInspection = Boolean.parseBoolean(parts[5]);
+                cars.add(new Car(number, brand, cost, power, mileage, techInspection));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("Техосмотр пройден для самой дорогой машины: " + mostExpensiveCar.isTechInspectionPassed());
+    }
 
-        // Найти мощность и максимальную высоту полета заданного самолета
-        String searchAirplaneNumber = "P5678";
-        for (Airplane airplane : airplanes) {
-            if (airplane.getNumber().equals(searchAirplaneNumber)) {
-                System.out.println("Мощность: " + airplane.getPower() + ", Макс. высота полета: " + airplane.getMaxFlightHeight());
-                break;
+    private static void readAirplanesFromFile(String filename, List<Airplane> airplanes) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                String number = parts[0];
+                String brand = parts[1];
+                double cost = Double.parseDouble(parts[2]);
+                double power = Double.parseDouble(parts[3]);
+                double maxFlightHeight = Double.parseDouble(parts[4]);
+                airplanes.add(new Airplane(number, brand, cost, power, maxFlightHeight));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        // Подсчет налога с регистрации всех самолетов (3% стоимости)
-        double airplaneTax = 0;
-        for (Airplane airplane : airplanes) {
-            airplaneTax += airplane.getCost() * 0.03;
-        }
-        System.out.println("Налог с регистрации всех самолетов: " + airplaneTax);
-
-        // Определить мощность, стоимость и марку самого дорогого самолета
-        Airplane mostExpensiveAirplane = airplanes.get(0);
-        for (Airplane airplane : airplanes) {
-            if (airplane.getCost() > mostExpensiveAirplane.getCost()) {
-                mostExpensiveAirplane = airplane;
+    private static void writeCarsNotPassedTechInspectionToFile(String filename, List<Car> cars) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (Car car : cars) {
+                bw.write(car.info());
+                bw.newLine();
             }
-        }
-        System.out.println("Самый дорогой самолет: " + mostExpensiveAirplane.getBrand() +
-                ", Стоимость: " + mostExpensiveAirplane.getCost() +
-                ", Мощность: " + mostExpensiveAirplane.getPower());
-
-        // Получение полной информации обо всех автомобилях и самолетах
-        System.out.println("\nИнформация обо всех автомобилях:");
-        for (Car car : cars) {
-            System.out.println(car.info());
-        }
-
-        System.out.println("\nИнформация обо всех самолетах:");
-        for (Airplane airplane : airplanes) {
-            System.out.println(airplane.info());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
